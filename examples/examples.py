31d980b8fd3d57e39f9f466b2b1b5bbddfd27ad3
Egor <egorobidin@inbox.ru>
2023-08-29 16:55:27 +0300

    second commit

--- examples/examples.py
+++ examples/examples.py
@@ -75,19 +75,71 @@ if __name__ == "__main__":
         "password": "123"
     }
 
-    from rest_framework_simplejwt.views import TokenObtainPairView, TokenVerifyView
+    from rest_framework_simplejwt.views import TokenObtainPairView, TokenBlacklistView
+    from rest_framework_simplejwt.tokens import AccessToken, RefreshToken, OutstandingToken, BlacklistedToken
     # Обработчик JWT для получения токена
     token_view = TokenObtainPairView.as_view()
     # Передаём запрос на получение токена для пользователя
     request = factory.post('/token/', user_data, format='json')
     response = token_view(request)
+    # Как только отработал обработчик создания токена, то в таблице Outstanding tokens БД создался действующий токен
     token_data = response.data
     # Токен доступа
-    access_token = token_data.get("access")  # 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjkyOTczNTkzLCJpYXQiOjE2OTI5NzMyOTMsImp0aSI6IjViNWRhZDA0Yjg0ODQxOTRiODdiOGFhM2EyN2E0MTI5IiwidXNlcl9pZCI6MX0.8Ea7HDi5IyZqiQxwXjo6u50MZQW5rnoKO0xRPc-ifNc'
-    # Проверка токена
-    request = factory.post('/token/verify/', {"token": access_token}, format='json')
-    refresh_view = TokenVerifyView.as_view()
-    response = refresh_view(request)
-    print(response.status_code)  # 200
+    access_token1 = token_data.get("access")
+    refresh_token1 = token_data.get("refresh")
+
+    # Работать с действующими токенами можно так(как с объектом модели)
+    print(OutstandingToken.objects.all())  # <QuerySet [
+    # <OutstandingToken: Token for admin (64363557b94b4253bbd95f029df03e01)>
+    # ]>
+
+    # В действующем токене хранится информация только о токене обновления. Библиотека подразумевает, что токен доступа
+    # сохранит или пользователь или приложение пользователя. Вспомните как GitHub выдаёт свои токены для подключения IDE
+
+    # Проверка, что в объекте БД хранится токен обновления
+    print(refresh_token1 == OutstandingToken.objects.first().token)  # True
+
+    # Создадим ещё пару токенов, чтобы потом добавить их в черный список разными способами
+    request = factory.post('/token/', user_data, format='json')
+    access_token2 = token_view(request).data.get("access")
+
+    request = factory.post('/token/', user_data, format='json')
+    access_token3 = token_view(request).data.get("access")
+
+    print(OutstandingToken.objects.all())  # <QuerySet [
+    # <OutstandingToken: Token for admin (64363557b94b4253bbd95f029df03e01)>,
+    # <OutstandingToken: Token for admin (123ebcc07c1e40f6968d005f181ec908)>,
+    # <OutstandingToken: Token for admin (ad249807841b4112a2c49ff7ca016116)>
+    # ]>
+
+    # access_token1, 2, 3 были сохранены специально, чтобы можно было проверить доступ к API, так как писалось ранее
+    # через токен обновления можно только получить новый токен, а не узнать текущий.
+
+    # 1. Использование класса представления TokenBlacklistView
+    token_blacklist_view = TokenBlacklistView.as_view()
+    # Просто отправляем запрос с доступом и токен попадёт в черный список автоматически
+    request = factory.post('/blacklist/', {'token': access_token1, 'refresh': refresh_token1}, format='json')
+
+    # Запомним jti (JSON Token ID) чтобы потом проверить, что токен есть в базе данных черного списка. Делается это
+    # заранее, так как если вызвать после блокировки, то вызовется исключение, что токен заблокирован
+    data_token = RefreshToken(refresh_token1)  # декодируем токен и получаем информацию
+    print(data_token)  # {'token_type': 'refresh', 'exp': 1693401757, 'iat': 1693315357,
+    # 'jti': 'f3d227af785d426b8c2b506700269693', 'user_id': 1}
+
+    # Отработаем представление
+    response = token_blacklist_view(request)
+
+    # Проверка, что токен теперь в БД черного списка
+
+    print(BlacklistedToken.objects.first().token.jti == data_token['jti'])
+
+
+
+
+
+
+
+    #
+    # OutstandingToken.objects.all().delete()
 
 
